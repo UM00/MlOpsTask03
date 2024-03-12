a27d82e56e3910cfd2834a5a5bd00d5525b60bcc
UMOO <umer.idrees.1906@gmail.com>
2024-03-12 20:19:07 +0500

    Jenkins Filed added

---
+++ README.md
@@ -0,0 +1,3 @@
+# MLOPS_20-2-24
+This is Repository in for CS-B class Wednesday 2/20/24
+We have automated PyTest using makeFIles
---
+++ main.py
@@ -0,0 +1,11 @@
+	def __init__(self,totalStudents):
+		   self.totalStudents = totalStudents
+	def getTotalStudents(self):
+		   return self.totalStudents
+	def addStudents(self):
+		   self.totalStudents = self.totalStudents + 1
+	def removeStudents(self):
+		   self.totalStudents = self.totalStudents - 1
+	def getClassName(self):
+		   return "Machine Learning Operations (CS-B)"
+
